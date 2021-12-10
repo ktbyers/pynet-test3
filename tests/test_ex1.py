469754c8fcce01059d67a300eab4a9b31bc8b979
Kirk Byers <ktbyers@twb-tech.com>
2021-12-10 10:29:31 -0800

    Adding unit tests

---
+++ tests/test_ex1.py
@@ -0,0 +1,24 @@
+import pytest
+
+
+def func1(x, y):
+    return x + y
+
+
+def func2(x, y):
+    return x * y
+
+
+def test_sum():
+    assert func1(10, 99) == 109
+
+
+def test_product():
+    assert func2(7, 9) == 63
+
+
+@pytest.mark.parametrize(
+    "num1, num2, result", [(77, 0, 0), (13, 13, 169), (-1, -1, 1), (-7, 7, -49)]
+)
+def test_product_params(num1, num2, result):
+    assert func2(num1, num2) == result
